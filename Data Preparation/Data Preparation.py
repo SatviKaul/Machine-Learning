32c968f9cf559df3c3f94e0655c82604a5b875cd
SatviKaul <satvik.1799@gmail.com>
2019-07-14 12:55:03 +0530

    Update Data Preparation.py

--- Data Preparation/Data Preparation.py
+++ Data Preparation/Data Preparation.py
@@ -3,7 +3,7 @@
 
 # # Importing the Libraries
 
-# In[1]:
+# In[3]:
 
 
 import numpy as np
@@ -13,37 +13,37 @@ import pandas as pd
 
 # # Importing the Dataset 
 
-# In[2]:
+# In[4]:
 
 
 dataset = pd.read_csv("Data.csv")
 
 
-# In[3]:
+# In[5]:
 
 
 X = dataset.iloc[:,:-1].values
 
 
-# In[5]:
+# In[6]:
 
 
 # iloc[: = all rows, :-1 = all columns except last].values = all values
 
 
-# In[6]:
+# In[7]:
 
 
 X
 
 
-# In[7]:
+# In[8]:
 
 
 y = dataset.iloc[:,3].values
 
 
-# In[8]:
+# In[9]:
 
 
 y
@@ -51,19 +51,19 @@ y
 
 # # Missing Data
 
-# In[9]:
+# In[10]:
 
 
 from sklearn.preprocessing import Imputer
 
 
-# In[10]:
+# In[11]:
 
 
 imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
 
 
-# In[11]:
+# In[12]:
 
 
 # missing_values = identifying missing values in dataset
@@ -71,13 +71,13 @@ imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
 # axis = 0 for columns, 1 for rows
 
 
-# In[16]:
+# In[13]:
 
 
 imputer = imputer.fit(X[:, 1:3])
 
 
-# In[13]:
+# In[14]:
 
 
 # .fit(data[rows, columns])
@@ -89,13 +89,13 @@ imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 
-# In[17]:
+# In[16]:
 
 
 # transform replaces values
 
 
-# In[18]:
+# In[17]:
 
 
 X 
@@ -103,25 +103,25 @@ X
 
 # # Categorical Data
 
-# In[24]:
+# In[18]:
 
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
-# In[20]:
+# In[19]:
 
 
 labelencoder_X = LabelEncoder()
 
 
-# In[22]:
+# In[20]:
 
 
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 
 
-# In[23]:
+# In[21]:
 
 
 X
@@ -133,31 +133,31 @@ X
 
 # #### This ensures above condition is nullified, and the number of columns is equal to the number of labels
 
-# In[25]:
+# In[22]:
 
 
 onehotencoder = OneHotEncoder(categorical_features = [0])
 
 
-# In[26]:
+# In[23]:
 
 
 X = onehotencoder.fit_transform(X).toarray()
 
 
-# In[27]:
+# In[24]:
 
 
 X
 
 
-# In[28]:
+# In[25]:
 
 
 labelencoder_y = LabelEncoder()
 
 
-# In[29]:
+# In[26]:
 
 
 y = labelencoder_y.fit_transform(y)
@@ -165,23 +165,47 @@ y = labelencoder_y.fit_transform(y)
 
 # # Test and Train Split
 
-# In[30]:
+# In[27]:
 
 
 from sklearn.model_selection import train_test_split
 
 
-# In[31]:
+# In[28]:
 
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
-# In[ ]:
+# # Feature Scaling
+
+# In[29]:
 
 
+from sklearn.preprocessing import StandardScaler
 
 
+# In[30]:
+
+
+sc_X = StandardScaler()
+
+
+# In[31]:
+
+
+X_train = sc_X.fit_transform(X_train)
+
+
+# In[33]:
+
+
+X_test = sc_X.transform(X_test)
+
+
+# ##### Do we need to transform dummy variables or scale them?
+
+# ##### It depends on the context, how much we wanna interpret our models. Won't break the model, but will damage interpretation.
 
 # In[ ]:
 
